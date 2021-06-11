e9ba9fd4bae38ad64c4170e1aa87f098e3192b8f
Hara Nguyen <ngttnguyen@gmail.com>
2021-06-11 19:16:57 +0700

    update

--- assignment_6/my_datacracy_slack_secret.py
+++ assignment_6/my_datacracy_slack_secret.py
@@ -11,8 +11,8 @@ import pandas as pd
 import numpy as np
 # import re
 from datetime import datetime as dt
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+# from pandas_profiling import ProfileReport
+# from streamlit_pandas_profiling import st_profile_report
 from matplotlib import pyplot as plt
 import seaborn as sns
 
